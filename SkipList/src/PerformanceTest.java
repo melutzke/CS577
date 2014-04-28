82376710133fa61cb4ca8addf8f51c38bca1dd1e
melutzke <mitchell649@hotmail.com>
2014-04-27 21:21:08 -0700

    Where's my dingus?!?

---
+++ SkipList/src/PerformanceTest.java
@@ -0,0 +1,22 @@
+package com.heymitch.skiplist;
+
+public class PerformanceTest {
+
+	public static void main(String[] args) {
+		ConcurrentSkipList<Integer> skiplist = new ConcurrentSkipList<Integer>();
+		
+		for(int i = 0; i < 10000; i+=2){
+			skiplist.add(i);
+		}
+		
+		for(int i = 0; i < 10000; i++){
+			if(skiplist.contains(i)){
+				System.out.println("Found " + i);
+			} else {
+				System.out.println("Missing " + i);
+			}
+		}
+
+	}
+
+}
